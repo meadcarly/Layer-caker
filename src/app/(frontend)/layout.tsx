9ae065aec5a96d78a7ee8d6d5eca40da3d32ef7e
Carly Mead <meadcarly16@gmail.com>
2024-11-20 10:26:00 -0700

    Working on a Sanity project following Content-driven web application foundations

---
+++ src/app/(frontend)/layout.tsx
@@ -0,0 +1,15 @@
+import '@/app/globals.css'
+import { SanityLive } from '@/sanity/lib/live'
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <>
+      {children}
+      <SanityLive />
+    </>
+  )
+}
